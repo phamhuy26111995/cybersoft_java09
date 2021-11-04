b33d280f5e51c9619d31551f8eed618bb229d4bd
phamhuy26111995 <67215173+phamhuy26111995@users.noreply.github.com>
2021-11-04 09:37:12 +0700

    Delete Final Layout/Elearning_Layout directory
    Delete Duplicate

--- Final Layout/Elearning_Layout/assets/js/login/index.js
+++
@@ -1,7 +0,0 @@
-//Hàm kiểm tra nếu ko có token thì đưa sang về lại trang login
-function sendRedirect(){
-    if(localStorage.getItem("USER_TOKEN")===null){
-        location.replace("/login.html");
-    }
-}
-sendRedirect();
