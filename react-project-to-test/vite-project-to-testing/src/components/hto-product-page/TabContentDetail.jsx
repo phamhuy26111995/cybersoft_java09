f6f82368c7177c4a67d163cf44b00f17d66c1197
Stephen Pham <Stephen.Pham@code88.asia>
2023-11-23 15:48:41 +0700

    update TabContentDetail

--- react-project-to-test/vite-project-to-testing/src/components/hto-product-page/TabContentDetail.jsx
+++ react-project-to-test/vite-project-to-testing/src/components/hto-product-page/TabContentDetail.jsx
@@ -11,9 +11,15 @@ function TabContentDetail(props) {
   const form = useContext(FormContext);
   const inputTypeProp = form.getFieldValue(tabContentKey)[keyProp] || "text";
   const [inputType, setInputType] = useState("text");
+  const [toggleValue, setToggleValue] = useState(false);
 
   function onChangeType(value) {
     setInputType(value);
+  }
+
+  function onToggleValue(value) {
+    console.log(value);
+    setToggleValue(value)
   }
 
   useEffect(() => {
@@ -21,44 +27,48 @@ function TabContentDetail(props) {
   }, [inputTypeProp]);
 
   return (
-    <div>
-      <Popconfirm
-        title="Bạn có chắc chắn muốn đóng tab này không?"
-        onConfirm={() => remove(keyProp)}
-        okText="Có"
-        cancelText="Không"
-        placement="bottomLeft"
-      >
-        <MinusCircleOutlined />
-      </Popconfirm>
-      <Form.Item
-        name={[keyProp, "title"]}
-        noStyle
-        rules={[{ required: true, message: "Vui lòng nhập tên đầu tiên" }]}
-      >
-        <Input placeholder="Tiêu đề" />
-      </Form.Item>
-      <Form.Item
-        noStyle
-        name={[keyProp, "content"]}
-        rules={[{ required: true, message: "Vui lòng nhập họ" }]}
-      >
-        {inputType === "text" ? (
-          <Input placeholder="Nội dung" />
-        ) : (
-          <TextArea placeholder="Nội dung" />
-        )}
-      </Form.Item>
-      <Form.Item noStyle name={[keyProp, "type"]} initialValue={inputType}>
-        <Select onChange={onChangeType}>
-          <Option value="text">Text</Option>
-          <Option value="textarea">TextArea</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item noStyle name={[keyProp, "tabContentId"]}>
-        <Input disabled width={50} />
-      </Form.Item>
-    </div>
+    <>
+      <div style={{ display: "flex", alignItems: "center", width: 500 }}>
+        <Popconfirm
+          title="Bạn có chắc chắn muốn đóng tab này không?"
+          onConfirm={() => remove(keyProp)}
+          okText="Có"
+          cancelText="Không"
+          placement="bottomLeft"
+        >
+          <MinusCircleOutlined />
+        </Popconfirm>
+        <Form.Item
+          name={[keyProp, "title"]}
+          noStyle
+          rules={[{ required: true, message: "Vui lòng nhập tên đầu tiên" }]}
+        >
+          {toggleValue || !inputTypeProp.title  ? (
+            <Input onBlur={() => onToggleValue(false)} placeholder="Tiêu đề" />
+          ) : (
+            <span onClick={() => onToggleValue(true)}>{inputTypeProp.title}</span>
+          )}
+          {/* <span>{inputTypeProp.title}</span> */}
+        </Form.Item>
+        <Form.Item
+          noStyle
+          name={[keyProp, "content"]}
+          rules={[{ required: true, message: "Vui lòng nhập họ" }]}
+        >
+          {inputType === "text" ? (
+            <Input placeholder="Nội dung" />
+          ) : (
+            <TextArea placeholder="Nội dung" />
+          )}
+        </Form.Item>
+        <Form.Item noStyle name={[keyProp, "type"]} initialValue={inputType}>
+          <Select onChange={onChangeType}>
+            <Option value="text">Text</Option>
+            <Option value="textarea">TextArea</Option>
+          </Select>
+        </Form.Item>
+      </div>
+    </>
   );
 }
 
